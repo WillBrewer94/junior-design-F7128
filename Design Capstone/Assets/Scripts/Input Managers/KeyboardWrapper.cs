8570df6abeedf742b69843cf6f02dc1d1d80cf73
Will Brewer <wbrewer6@gatech.edu>
2017-10-01 21:31:33 -0400

    Added a LOT of code that will help translate inputs across differing devices and operating systems

---
+++ Design Capstone/Assets/Scripts/Input Managers/KeyboardWrapper.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardWrapper : ControllerInputWrapper 
+{
+
+	public enum MouseControlMode {None, Drag, Offset};
+
+	public static MouseControlMode mouseControlMode = MouseControlMode.Drag;
+
+	private bool dragStarted;
+	private Vector3 prevMousePos = Vector3.zero;
+	private Vector2 dragDifference = Vector2.zero;
+
+	public KeyboardWrapper(int joyNum) : base(joyNum)
+    {
+		
+    }
+
+    public override float GetAxis(Axis axis, bool isRaw = false)
+    {
+        string axisName = "";
+        float scale = 1;
+        Vector3 vec = Vector3.zero;
+        switch (axis)
+        {
+            case Axis.LeftStickX:
+                axisName = GetAxisName("Horizontal", "Horizontal", "Horizontal");
+                break;
+            case Axis.LeftStickY:
+                axisName = GetAxisName("Vertical", "Vertical", "Vertical");
+                break;
+            case Axis.RightStickX:
+                vec = retrieveMouseOffset();
+                return vec.normalized.x;
+            case Axis.RightStickY:
+                vec = retrieveMouseOffset();
+				return vec.normalized.y;
+			case Axis.DPadX:
+				axisName = GetAxisName("DPadX", "DPadX", "DPadX");
+//				scale = 0.09f;
+				break;
+			case Axis.DPadY:
+				axisName = GetAxisName("DPadY", "DPadY", "DPadY");
+//				scale = 0.09f;
+				break;
+        }
+        return Input.GetAxis(axisName) * scale;
+    }
+
+    Vector3 retrieveMouseOffset()
+    {
+		switch (mouseControlMode) {
+			case MouseControlMode.None:
+				return Vector3.zero;
+			case MouseControlMode.Drag:
+				if(Input.GetMouseButtonDown(0)) {
+					prevMousePos = Input.mousePosition;
+					dragStarted = true;
+				} else if(!Input.GetMouseButton(0)) {
+					dragStarted = false;
+				}
+				if(dragStarted) {
+					dragDifference = new Vector2(Input.mousePosition.x - prevMousePos.x, Input.mousePosition.y - prevMousePos.y);
+				} else {
+					dragDifference = Vector2.zero;
+				}
+				return dragDifference;
+			case MouseControlMode.Offset:
+				dragDifference += new Vector2(Input.mousePosition.x - prevMousePos.x, Input.mousePosition.y - prevMousePos.y);
+				prevMousePos = Input.mousePosition;
+				return dragDifference;
+		}
+		return Vector3.zero;
+    }
+
+    public override float GetTrigger(Triggers trigger, bool isRaw = false)
+    {
+        string triggerName = "";
+        switch (trigger)
+        {
+            case Triggers.RightTrigger:
+				triggerName = GetButtonName("RightTrigger", "RightTrigger", "RightTrigger");
+                break;
+            case Triggers.LeftTrigger:
+				triggerName = GetButtonName("LeftTrigger", "LeftTrigger", "LeftTrigger");
+                break;
+        }
+
+        if (Input.GetButton(triggerName))
+        {
+            return 1f;
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+
+    public override bool GetButton(Buttons button)
+    {
+		string buttonName = GetButtonHelper(button);
+		return Input.GetButton(buttonName);
+    }
+
+	public override bool GetButtonDown(Buttons button)
+	{
+		string buttonName = GetButtonHelper(button);
+		return Input.GetButtonDown(buttonName);
+	}
+
+    public override bool GetButtonUp(Buttons button)
+    {
+        return false;
+    }
+
+    protected override string GetAxisName(string winID, string linID, string osxID)
+    {
+        string axisName = "k" + "_Axis_" + winID;
+
+        return axisName;
+    }
+
+    protected override string GetButtonName(string winID, string linID, string osxID)
+    {
+        string buttonName = "k" + "_Button_" + winID;
+        return buttonName;
+    }
+
+	public override string GetButtonHelper (Buttons button)
+	{
+		string buttonName = "";
+
+		switch (button)
+		{
+		case Buttons.RightBumper:
+			buttonName = GetButtonName("RB", "RB", "RB");
+			break;
+		case Buttons.LeftBumper:
+			buttonName = GetButtonName("LB", "LB", "LB");
+			break;
+		case Buttons.A:
+			buttonName = GetButtonName("A", "A", "A");
+			break;
+		case Buttons.B:
+			buttonName = GetButtonName("B", "B", "B");
+			break;
+		case Buttons.X:
+			buttonName = GetButtonName("X", "X", "X");
+			break;
+		case Buttons.Y:
+			buttonName = GetButtonName("Y", "Y", "Y");
+			break;
+		case Buttons.Start:
+			buttonName = GetButtonName("Start", "Start", "Start");
+			break;
+		case Buttons.LeftStickClick:
+			buttonName = GetButtonName("LeftStickClick", "LeftStickClick", "LeftStickClick");
+			break;
+		case Buttons.RightStickClick:
+			buttonName = GetButtonName("RightStickClick", "RightStickClick", "RightStickClick");
+			break;
+		case Buttons.Back:
+			buttonName = GetButtonName("Back", "Back", "Back");
+			break;
+		}
+
+		return buttonName;
+	}
+}
---
+++ Design Capstone/Assets/Scripts/Input Managers/PS4ControllerWrapper.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PS4ControllerWrapper : ControllerInputWrapper {
+
+    public PS4ControllerWrapper(int joyNum) : base(joyNum) {
+
+    }
+
+    public override float GetAxis(Axis axis, bool isRaw = false) {
+        float scale = 1;
+        string axisName = "";
+
+        switch(axis) {
+            case Axis.LeftStickX:
+                axisName = GetAxisName("X", "X", "X");
+                break;
+            case Axis.LeftStickY:
+                axisName = GetAxisName("Y", "Y", "Y");
+                scale = -1;
+                break;
+            case Axis.RightStickX:
+                axisName = GetAxisName("3", "3", "3");
+                break;
+            case Axis.RightStickY:
+                axisName = GetAxisName("6", "4", "4");
+                scale = -1;
+                break;
+            case Axis.DPadX:
+                axisName = GetAxisName("7", "7", "7");
+                break;
+            case Axis.DPadY:
+                axisName = GetAxisName("8", "8", "8");
+                if(ControllerManager.instance.currentOS != ControllerManager.OperatingSystem.Win)
+                    scale = -1;
+                break;
+        }
+
+        if(isRaw) {
+            return Input.GetAxisRaw(axisName) * scale;
+        } else {
+            return Input.GetAxis(axisName) * scale;
+        }
+    }
+
+    public override bool GetButton(Buttons button) {
+        string buttonName = GetButtonHelper(button);
+        if(buttonName != null) {
+            return Input.GetButton(buttonName);
+        }
+        return false;
+    }
+
+    public override bool GetButtonDown(Buttons button) {
+        string buttonName = GetButtonHelper(button);
+        if(buttonName != null) {
+            return Input.GetButtonDown(buttonName);
+        }
+        return false;
+    }
+
+    public override string GetButtonHelper(Buttons button) {
+        string buttonName = "";
+        switch(button) {
+            case Buttons.A:
+                buttonName = GetButtonName("1", "1", "1");
+                break;
+            case Buttons.B:
+                buttonName = GetButtonName("2", "2", "2");
+                break;
+            case Buttons.X:
+                buttonName = GetButtonName("0", "0", "0");
+                break;
+            case Buttons.Y:
+                buttonName = GetButtonName("3", "3", "3");
+                break;
+            case Buttons.LeftBumper:
+                buttonName = GetButtonName("4", "4", "4");
+                break;
+            case Buttons.RightBumper:
+                buttonName = GetButtonName("5", "5", "5");
+                break;
+            case Buttons.LeftStickClick:
+                buttonName = GetButtonName("10", "10", "10");
+                break;
+            case Buttons.RightStickClick:
+                buttonName = GetButtonName("11", "11", "11");
+                break;
+            case Buttons.Start:
+                buttonName = GetButtonName("9", "9", "9");
+                break;
+            case Buttons.Back:
+                buttonName = GetButtonName("8", "8", "8");
+                break;
+        }
+        return buttonName;
+    }
+
+    public override bool GetButtonUp(Buttons button) {
+        string buttonName = GetButtonHelper(button);
+
+        return Input.GetButtonUp(buttonName);
+    }
+
+    public override float GetTrigger(Triggers trigger, bool isRaw = false) {
+        string triggerName = "";
+        switch(trigger) {
+            case Triggers.LeftTrigger:
+                triggerName = GetAxisName("4", "5", "5");
+                break;
+            case Triggers.RightTrigger:
+                triggerName = GetAxisName("5", "6", "6");
+                break;
+        }
+        if(isRaw) {
+            return Input.GetAxisRaw(triggerName);
+        }
+        return Input.GetAxis(triggerName);
+    }
+}

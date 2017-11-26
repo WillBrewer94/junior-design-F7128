9e5a3e5f874e64ade2888a69394d566e7a69222c
blackjack605 <jacklwinski@gmail.com>
2017-11-26 18:56:00 -0500

    Revert "Revert "Database Access""
    This reverts commit f11baf2bebc4252a0bfa21a85be783ed29833cf0.

---
+++ Design Capstone/Assets/LoginUI/UserProfile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Azure.AppServices;
+
+public class UserProfile : DataModel {
+
+    public string username;
+    public string password;
+    public string email;
+
+    public UserProfile(string username, string password, string email)
+    {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UserProfile()
+    {
+        this.username = "test";
+        this.password = "test";
+        this.email = "test";
+    }
+}
